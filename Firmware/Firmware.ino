cdeaa10d8fcf435b0cda4a1d0c1dd99f45b7b7c9
adammhaile <adammhaile@gmail.com>
2016-12-15 18:26:38 -0500

    adding button press

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -1,6 +1,9 @@
 #include "TimerOne.h"
 #include "display.h"
 
+#define BTN_A 2
+#define BTN_B 3
+
 uint8_t _cur_value;
 byte _digit_values[DIGIT_COUNT];
 
@@ -52,12 +55,69 @@ void set_dp(byte digit, bool state){
     else _digit_values[digit] &= ~_BV(7);
 }
 
+void btn_a(){
+
+}
+
+void btn_b(){
+
+}
+
+void set_btn_inc_isr(bool state){
+    if(state){
+        attachInterrupt(digitalPinToInterrupt(BTN_A), btn_a, FALLING);
+        attachInterrupt(digitalPinToInterrupt(BTN_B), btn_b, FALLING);
+    }
+    else{
+        detachInterrupt(digitalPinToInterrupt(BTN_A));
+        detachInterrupt(digitalPinToInterrupt(BTN_B));
+    }
+
+}
+
+#define BTN_HOLD_TIME 1000
+bool last_btn_a, last_btn_b; //last btn state
+long t_btn_a, t_btn_b; //last btn press time
+bool f_btn_a, f_btn_b; //btn hold flag
+bool r_btn_a, r_btn_b; //btn reset flag
+inline void check_btn_hold(){
+    if(digitalRead(BTN_A) == LOW){
+        if(!f_btn_a){
+            if(!last_btn_a){
+                last_btn_a = true;
+                t_btn_a = millis();
+            }
+            else{
+                if(millis() - t_btn_a >= BTN_HOLD_TIME)
+                {
+                    f_btn_a = true;
+                }
+            }
+        }
+    }
+    else{
+        if(f_btn_a && r_btn_a){
+            f_btn_a = r_btn_a = last_btn_a = false;
+        }
+    }
+}
+
 void setup(){
+    Serial.begin(115200);
     set_value(0);
     //Setup display multiplex timer
-    Timer1.initialize(2500);
+    Timer1.initialize(1000);
     Timer1.attachInterrupt(plex);
 
+    //Setup btn pins
+    pinMode(BTN_A, INPUT_PULLUP);
+    pinMode(BTN_B, INPUT_PULLUP);
+    last_btn_a = last_btn_b = false;
+    t_btn_a = t_btn_b = 0;
+    f_btn_a = f_btn_b = false;
+    r_btn_a = r_btn_b = false;
+
+    //Initialize 7-seg displays
     byte i = 0;
     for(i=0; i<DIGIT_COUNT; i++){
         pinMode(cathodes[i], OUTPUT);
@@ -67,20 +127,31 @@ void setup(){
         pinMode(anodes[i], OUTPUT);
         digitalWrite(anodes[i], LOW);
     }
+
+    set_value(0);
+    set_dp(0, true);
+    set_dp(1, true);
 }
 
 uint8_t test_val = 0;
+#define HOLD_BTN_A f_btn_a && !r_btn_a
+#define HOLD_BTN_B f_btn_b && !r_btn_b
 void loop(){
-    set_value(test_val);
-    set_dp(0, test_val % 2 == 0);
-    set_dp(1, test_val % 2 != 0);
-    delay(200);
-    test_val += 1;
-    if(test_val >= 9) {
-        test_val = 0;
-        _pwm_level -= 2;
-        if(_pwm_level == 0 || _pwm_level > PWM_STEPS){
-            _pwm_level = PWM_STEPS;
-        }
+    check_btn_hold();
+    if(HOLD_BTN_A){
+        set_value(++test_val);
+        r_btn_a = true;
     }
+    // set_value(test_val);
+    // set_dp(0, test_val % 2 == 0);
+    // set_dp(1, test_val % 2 != 0);
+    // delay(200);
+    // test_val += 1;
+    // if(test_val >= 9) {
+    //     test_val = 0;
+    //     _pwm_level -= 2;
+    //     if(_pwm_level == 0 || _pwm_level > PWM_STEPS){
+    //         _pwm_level = PWM_STEPS;
+    //     }
+    // }
 }
