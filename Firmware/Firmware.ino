441701ee94b67bd69a9253da19822de3f1930012
adammhaile <adammhaile@gmail.com>
2016-12-17 10:59:23 -0500

    working buttons

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -2,6 +2,7 @@
 #include "TimerOne.h"
 #include "display.h"
 #include "buttons.h"
+#include "MsTimer2.h"
 
 uint8_t _cur_value;
 byte _digit_values[DIGIT_COUNT];
@@ -42,8 +43,8 @@ inline void disp_num(byte digit){
 #define PWM_STEPS 20
 #define PWM_HIGH 20
 #define PWM_LOW 5
-byte _pwm_step = 0;
-byte _digit = 0;
+volatile byte _pwm_step = 0;
+volatile byte _digit = 0;
 void plex(){
     static byte i;
     //disable ALL to prevent ghosting
@@ -65,6 +66,7 @@ void plex(){
 
 }
 
+
 void set_dp(byte digit, bool state){
     if(state) _digit_values[digit] |= _BV(7);
     else _digit_values[digit] &= ~_BV(7);
@@ -82,35 +84,68 @@ void set_value(uint8_t value){
     }
 }
 
-inline void check_btn_hold(_button *btn){
-    if(digitalRead(btn->pin) == LOW){
-        if(!btn->flag){
-            if(!btn->last_state){
-                btn->last_state = true;
-                btn->time_press = millis();
-            }
-            else{
-                if(millis() - btn->time_press >= BTN_HOLD_TIME)
-                {
-                    btn->flag = true;
-                }
-            }
+// inline void check_btn_hold(_button *btn){
+//     if(digitalRead(btn->pin) == LOW){
+//         if(!btn->flag){
+//             if(!btn->last_state){
+//                 btn->last_state = true;
+//                 btn->time_press = millis();
+//             }
+//             else{
+//                 if(millis() - btn->time_press >= BTN_HOLD_TIME)
+//                 {
+//                     btn->flag = true;
+//                 }
+//             }
+//         }
+//     }
+//     else{
+//         if(btn->flag && btn->reset){
+//             btn->flag = btn->reset = false;
+//         }
+//         btn->last_state = false;
+//     }
+// }
+
+inline void check_btn(_button *btn){
+    static bool state;
+    state = (digitalRead(btn->pin) == LOW);
+    if(!state && btn->hold_flag && btn->reset){
+        btn->hold_clear = true;
+    }
+    else if(!(btn->hold_flag || btn->press_flag)){
+        if(state){
+            btn->count++;
+            if(btn->count >= BTN_HOLD_TIME / BTN_SCAN_PERIOD) btn->hold_flag = true;
+        }
+        else{
+            if(btn->count >= BTN_PRESS_TIME / BTN_SCAN_PERIOD) btn->press_flag = true;
         }
     }
-    else{
-        if(btn->flag && btn->reset){
-            btn->flag = btn->reset = false;
+
+    if(btn->hold_flag || btn->press_flag) btn->count = 0;
+}
+
+inline void clear_btn(_button *btn){
+    if(btn->reset){
+        if(btn->press_flag){
+            btn->press_flag = btn->reset = false;
         }
-        btn->last_state = false;
+        else if(btn->hold_flag && btn->hold_clear){
+            btn->hold_flag = btn->reset = btn->hold_clear = false;
+        }
     }
 }
 
-inline void check_buttons(){
-    check_btn_hold(&BTN_A);
-    check_btn_hold(&BTN_B);
+void check_btns(){
+    check_btn(&BTN_A);
+    check_btn(&BTN_B);
 }
 
-#define BTN_PRESS_TIME 5
+void clear_btns(){
+    clear_btn(&BTN_A);
+    clear_btn(&BTN_B);
+}
 
 long _t_btn_a;
 void btn_a(){
@@ -166,10 +201,6 @@ void setup(){
     //load config
     read_config();
 
-    //Setup display multiplex timer
-    Timer1.initialize(500);
-    Timer1.attachInterrupt(plex);
-
     //Setup btn pins
     pinMode(BTN_A.pin, INPUT_PULLUP);
     pinMode(BTN_B.pin, INPUT_PULLUP);
@@ -184,56 +215,66 @@ void setup(){
         pinMode(anodes[i], OUTPUT);
         digitalWrite(anodes[i], LOW);
     }
+
+    //Setup timer for plex
+    Timer1.initialize(500);
+    Timer1.attachInterrupt(plex);
+
+    //Setup timer for buttons
+    MsTimer2::set(BTN_SCAN_PERIOD, check_btns);
+    MsTimer2::start();
 }
 
 uint8_t test_val = 0;
 bool _in_target_set = false;
 
 void loop(){
-    check_buttons();
     if(HOLD_BTN_A){
-        if(_in_target_set){
-            _in_target_set = false;
-            set_btn_inc_isr(false);
-            set_dp(0, false);
-            set_dp(1, false);
-        }
-        else{
-            _in_target_set = true;
-            set_btn_inc_isr(true);
-            set_dp(0, true);
-            set_dp(1, true);
-        }
+        Serial.println("HOLD A");
         BTN_A.reset = true;
     }
-    if(!_in_target_set){
-        if(HOLD_BTN_B){
-            _pwm_level = !_pwm_level;
-            write_config();
-            BTN_B.reset = true;
-        }
+
+    if(PRESS_BTN_A){
+        Serial.println("PRESS A");
+        BTN_A.reset = true;
     }
 
-    if(_in_target_set){
-        // Serial.println(_target_speed);
-        set_value(_target_speed);
-        // delay(250);
-    }
-    else{
-        set_value(test_val++);
-        if(test_val > 99) test_val = 0;
-        delay(200);
-    }
-    // set_value(test_val);
-    // set_dp(0, test_val % 2 == 0);
-    // set_dp(1, test_val % 2 != 0);
-    // delay(200);
-    // test_val += 1;
-    // if(test_val >= 9) {
-    //     test_val = 0;
-    //     _pwm_level -= 2;
-    //     if(_pwm_level == 0 || _pwm_level > PWM_STEPS){
-    //         _pwm_level = PWM_STEPS;
-    //     }
-    // }
+    clear_btns();
 }
+
+// void loop(){
+//     // check_buttons();
+//     if(HOLD_BTN_A){
+//         if(_in_target_set){
+//             _in_target_set = false;
+//             // set_btn_inc_isr(false);
+//             set_dp(0, false);
+//             set_dp(1, false);
+//         }
+//         else{
+//             _in_target_set = true;
+//             // set_btn_inc_isr(true);
+//             set_dp(0, true);
+//             set_dp(1, true);
+//         }
+//         BTN_A.reset = true;
+//     }
+//     if(!_in_target_set){
+//         if(HOLD_BTN_B){
+//             _pwm_level = !_pwm_level;
+//             write_config();
+//             BTN_B.reset = true;
+//         }
+//     }
+//
+//     if(_in_target_set){
+//         // Serial.println(_target_speed);
+//         set_value(_target_speed);
+//         // delay(250);
+//     }
+//     else{
+//         set_value(test_val++);
+//         if(test_val > 99) test_val = 0;
+//         delay(200);
+//     }
+// }
--- Firmware/buttons.h
+++ Firmware/buttons.h
@@ -1,30 +1,37 @@
 typedef struct {
     byte pin;
-    bool last_state;
-    long time_press;
-    bool flag;
+    uint16_t count;
+    bool hold_flag;
+    bool hold_clear;
+    bool press_flag;
     bool reset;
 } _button;
 
-_button btns[2] = {
+volatile _button btns[2] = {
     {   //Button A
         2, //pin
-        false, //last_state
-        0, //time_press
-        false, //flag
+        0, //count
+        false, //hold_flag
+        false, //hold_clear
+        false, //press_flag
         false, //reset
     },
     {   //Button B
         3, //pin
-        false, //last_state
-        0, //time_press
-        false, //flag
+        0, //count
+        false, //hold_flag
+        false, //hold_clear
+        false, //press_flag
         false, //reset
     }
 };
 
 #define BTN_A btns[0]
 #define BTN_B btns[1]
-#define BTN_HOLD_TIME 600
-#define HOLD_BTN_A BTN_A.flag && !BTN_A.reset
-#define HOLD_BTN_B BTN_B.flag && !BTN_B.reset
+#define BTN_SCAN_PERIOD 5
+#define BTN_HOLD_TIME 750
+#define BTN_PRESS_TIME 50
+#define HOLD_BTN_A BTN_A.hold_flag && !BTN_A.reset
+#define HOLD_BTN_B BTN_B.hold_flag && !BTN_B.reset
+#define PRESS_BTN_A BTN_A.press_flag && !BTN_A.reset
+#define PRESS_BTN_B BTN_B.press_flag && !BTN_B.reset
